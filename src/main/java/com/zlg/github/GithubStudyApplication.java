ae72eb3011cb0eca0996c43cdfff5e0b3280bd60
zlg <3303689849@qq.com>
2020-10-08 10:45:24 +0800

    new file hello.java

---
+++ src/main/java/com/zlg/github/GithubStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zlg.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zlg/github/Hello.java
@@ -0,0 +1,10 @@
+package com.zlg.github;
+
+/**
+ * @PROJECT_NAME: github-study
+ * @PACKAGE_NAME: com.zlg.github
+ * @USER: 33036
+ * @DATE: 2020/10/8
+ */
+public class Hello {
+}
